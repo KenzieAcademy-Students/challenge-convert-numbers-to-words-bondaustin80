9a9a7f1e9482594838f0afd6ed201fc6a9416b3e
Austin Bond <bondaustin80@gmail.com>
2021-08-17 09:22:37 -0400

    Made basic more readable

--- code.js
+++ code.js
@@ -1,12 +1,38 @@
 // Your code here
 
-//Basic
-
 let ones = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 let tens = ["", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety", "hundred"]
 
 let special = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+
+function convertOnes(x) {
+    let word = ""
+    word = ones[x - 1]
+    return word
+}
+
+function convertTens(x) {
+    let word = ""
+    word = tens[parseInt(x.toString().charAt(0))]
+    console.log(word)
+    if (parseInt(x.toString().charAt(1)) > 0) {
+        word = word + "-" + convertOnes(parseInt(x.toString().charAt(1)))
+    }
+    return word
+}
+
+function convertHundreds(x) {
+    let word = ""
+    word = ones[x - 1] + "-hundred"
+    return word
+}
+
+function convertSpecial(x){
+    let word = ""
+    word = special[x]
+    return word
+}
 
 function displayFirstHundred() {
     let count = ""
@@ -17,21 +43,18 @@ function displayFirstHundred() {
             count = count + ", "
         }
         if (number.length === 3) {
-            count = count + "hundred"
+            count = count + convertHundreds(parseInt(number.charAt(0)))
         } else {
             if (number.length === 2) {
                 if (number.charAt(0) === "1") {
-                    count = count + (special[parseInt(number.charAt(1))])
+                    count = count + convertSpecial(parseInt(number.charAt(1)))
                     continue //https://www.w3schools.com/jsref/jsref_continue.asp
                 } else {
-                    word = word + tens[parseInt(number.charAt(0))]
-                    if (parseInt(number.charAt(1)) > 0) {
-                        word = word + "-" + ones[parseInt(number.charAt(1)) - 1]
-                    }
+                    word = word + convertTens(parseInt(number))
                     count = count + word
                 }
             } else {
-                count = count + ones[parseInt(number) - 1]
+                count = count + convertOnes(parseInt(number))
             }
         }
     }
