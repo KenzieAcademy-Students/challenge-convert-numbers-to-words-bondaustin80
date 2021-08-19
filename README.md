b7ff56a3ab6865bce1e9778b93a5171dc072fd8a
kenzieadmin <kenzieadmin@users.noreply.github.com>
2021-08-19 15:22:15 +0000

    deploy: f2c92dbaaa46d11124d72b9afc4adfb27a35fe9e

--- README.md
+++ README.md
@@ -6,10 +6,22 @@ Customize this README.md however you want to turn it into documentation for your
 
 ## Project Plan
 
-_(Put your project plan here. It could be pseudocode, an outline-style development plan, etc. But whatever form you choose, it should be detailed enough that another developer could feasibly use it to implement your solution.)_
+My current project plan will be to create 3 arrays that contain common word rules. I'll need one for ones digits, one for tens digits, and one that contains numbers betwen 10 and 19 since those all follow their own rules. While this program will be able to count up to 1000000, hundreds and above all follow the same naming conventions, so they don't need their own arrays.
+
+The first step will be to create a few reusable converter functions in order to keep code clean and readable. These converter functions will be able to convert each number into a word based off of specific rules and using arrays, and some of them will even use other converters in order to save time and space.
+
+Finally, I will use a for loop that will push each number into an array. The way it will convert numbers is based off of the length of each number, the program will use if-else statements to determine which converter to use. After the converted numbers are return, they will be added to a single string, and the appended to the document.
 
 ## Reflection
 
 What different approaches or techniques did you consider when planning your implementation? What were the advantages and disadvantages of those alternatives?
+
+There were 3 different approaches I considered for running the code. 
+
+The first approach was to create a single string, and each word would be added to the end of the string. While the initial approach was very easy, it was not efficient for longer code, and would end up crashing the tab when I tried to create too large of a string.
+
+The second approach I used was to append each word to the document as they were added. While this was more efficient, more easily readable, and did not crash the tab, it still used too much memory and would consistently stop at 986895 if I started from 1.
+
+The final approach was to push each number to an array after conversion, and once the array had all 1000000 words, I created another for loop that would splice the words into groups of 10000, and word join and append each array seperately. While this was by far the most complex way of doing it, it ended up being the most efficient memory wise, making it the only way to get all 1000000 words on Chrome without crashing the browser.
 
 _(Put your reflection answer here.)_
--- code.js
+++ code.js
@@ -1,1 +1,170 @@
 // Your code here
+
+//Arrays for converters to reference
+
+let ones = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+let tens = ["", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety", "hundred"]
+
+let special = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+
+//Reusable converters that the functions will use to find numbers
+function convertOnes(x) {
+    let word = ""
+    word = ones[x - 1]
+    return word
+}
+
+function convertTens(x) {
+    let word = ""
+    word = tens[parseInt(x.toString().charAt(0))]
+    if (parseInt(x.toString().charAt(1)) > 0) {
+        if (parseInt)
+        word = word + "-" + convertOnes(parseInt(x.toString().charAt(1)))
+    } 
+    return word
+}
+
+function convertHundreds(x) {
+    let number = x.toString()
+    let word = ""
+    word = ones[parseInt(number.charAt(0)) - 1] + "-hundred"
+    if (parseInt(number.charAt(1)) > 0) {
+        if (parseInt(number.charAt(1)) === 1) {
+            word = word + "-" + convertSpecial(parseInt(number.substring(2))) //https://www.w3schools.com/jsref/jsref_substring.asp
+        } else {
+            word = word + "-" + convertTens(parseInt(number.substring(1)))
+        }
+    } else if (parseInt(number.charAt(2)) > 0) {
+        word = word + "-" + convertOnes(parseInt(number.charAt(2)))
+    }
+    return word
+}
+
+function convertThousands(x) {
+    let number = x.toString()
+    let word = ""
+    if (number.length === 4) {
+        word = word + convertOnes(parseInt(number.substring(0,1))) + "-thousand"
+    } else if (number.length === 5) {
+        if (parseInt(number.charAt(0)) === 1) {
+            word = convertSpecial(parseInt(number.charAt(1))) + "-thousand"
+            number = number.substring(1)
+        } else {
+            word = word + convertTens(parseInt(number.substring(0,2))) + "-thousand"
+            number = number.substring(1)
+        }
+    } else if (number.length === 6) {
+        word = word + convertHundreds(parseInt(number.substring(0,3))) + "-thousand"
+        number = number.substring(2)
+    }
+    if (parseInt(number.charAt(1)) > 0) {
+        word = word + "-" + convertHundreds(parseInt(number.substring(1)))
+    } else if (parseInt(number.charAt(2) === 1)) {
+        word = word + "-" + convertSpecial(parseInt(number.substring(3)))
+    } else if (parseInt(number.charAt(2)) > 2) {
+        word = word + "-" + convertTens(parseInt(number.substring(2)))
+    } else if (parseInt(number.charAt(3)) > 0) {
+        word = word + "-" + convertOnes(parseInt(number.substring(3)))
+    }
+    return word
+}
+
+function convertSpecial(x){
+    let word = ""
+    word = special[x]
+    return word
+}
+
+
+//Function to get array of words
+function countWords() {
+    let count = []
+    for (let i = 1; i <= 1000000; i++) {
+        let number = i.toString()
+        let word = ""
+        if (number.length === 7) {
+            count.push("one-million")
+        } else if (number.length >= 4 && number.length <= 6) {
+            word = convertThousands(parseInt(number))
+            count.push(word)
+        } else if (number.length === 3) {
+            word = convertHundreds(parseInt(number))
+            count.push(word)
+        } else if (number.length === 2) {
+            if (number.charAt(0) === "1") {
+                word = convertSpecial(parseInt(number.charAt(1)))
+                count.push(word)
+                continue //https://www.w3schools.com/jsref/jsref_continue.asp
+            } else {
+                word = word + convertTens(parseInt(number))
+                count.push(word)
+            }
+        } else if (number.length === 1) {
+            word = convertOnes(parseInt(number))
+            count.push(word)
+        }
+    }
+    let size = count.length/10000
+    for (let i = 0; i < size; i++) {
+        let temp = count.slice(0, 10000)
+        count.splice(0, 10000)
+        setTimeout(displayWords, 3000, temp)
+    }
+}
+
+//Function to display all numbers at once
+function displayWords(array) {
+        let display = document.createElement("p")
+        display.innerText = array.join(", ")
+        document.body.append(display)
+}
+
+//Function will use user input to find matching word
+function convertInput() {
+    let number = document.getElementById("inputNum").value
+    console.log(number)
+    let result = document.getElementById("result")
+    if (number < 1 || number > 1000000 || isNaN(number)) {
+        result.innerText = "Please enter valid number"
+    } else {
+        number = number.toString()
+        let word = ""
+        if (number.length === 7) {
+            result.innerText = "Result: one-million"
+        } else if (number.length >= 4 && number.length <= 6) {
+            word = convertThousands(parseInt(number))
+            result.innerText = "Result: " + word
+        } else if (number.length === 3) {
+            word = convertHundreds(parseInt(number))
+            result.innerText = "Result: " + word
+        } else if (number.length === 2) {
+            if (number.charAt(0) === "1") {
+                word = convertSpecial(parseInt(number.charAt(1)))
+                result.innerText = "Result: " + word
+            } else {
+                word = word + convertTens(parseInt(number))
+                result.innerText = "Result: " + word
+            }
+        } else if (number.length === 1) {
+            word = convertOnes(parseInt(number))
+            result.innerText = "Result: " + word
+        }
+    }
+}
+
+let inputNum = document.createElement("input")
+let title = document.createElement("label")
+let submit = document.createElement("button")
+let result = document.createElement("p")
+title.innerText = "Enter number between 1 and 1000000:"
+inputNum.id = "inputNum"
+result.id = "result"
+submit.innerText="Submit"
+submit.addEventListener("click", convertInput)
+document.body.append(title)
+document.body.append(inputNum)
+document.body.append(submit)
+document.body.append(result)
+
+countWords()
