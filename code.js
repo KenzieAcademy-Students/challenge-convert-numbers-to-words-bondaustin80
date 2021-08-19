418fe89617aa20b454c2d534a8f0c71f08f5da00
Austin Bond <bondaustin80@gmail.com>
2021-08-19 10:59:29 -0400

    Completed Advanced

--- code.js
+++ code.js
@@ -1,11 +1,14 @@
 // Your code here
 
+//Arrays for converters to reference
+
 let ones = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 let tens = ["", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety", "hundred"]
 
 let special = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 
+//Reusable converters that the functions will use to find numbers
 function convertOnes(x) {
     let word = ""
     word = ones[x - 1]
@@ -32,6 +35,8 @@ function convertHundreds(x) {
         } else {
             word = word + "-" + convertTens(parseInt(number.substring(1)))
         }
+    } else if (parseInt(number.charAt(2)) > 0) {
+        word = word + "-" + convertOnes(parseInt(number.charAt(2)))
     }
     return word
 }
@@ -64,7 +69,6 @@ function convertThousands(x) {
     }
     return word
 }
-console.log(convertThousands(257933))
 
 function convertSpecial(x){
     let word = ""
@@ -72,38 +76,95 @@ function convertSpecial(x){
     return word
 }
 
-function displayWords() {
-    let count = ""
-    for (let i = 1; i <= 100000; i++) {
+
+//Function to get array of words
+function countWords() {
+    let count = []
+    for (let i = 1; i <= 1000000; i++) {
         let number = i.toString()
         let word = ""
-        if (i > 1) {
-            count = count + ", "
-        }
-        if (number.length >= 4) {
+        if (number.length === 7) {
+            count.push("one-million")
+        } else if (number.length >= 4 && number.length <= 6) {
             word = convertThousands(parseInt(number))
-            count = count + word
-        }
-        if (number.length === 3) {
-            count = count + convertHundreds(parseInt(number))
-        }
-        if (number.length === 2) {
+            count.push(word)
+        } else if (number.length === 3) {
+            word = convertHundreds(parseInt(number))
+            count.push(word)
+        } else if (number.length === 2) {
             if (number.charAt(0) === "1") {
-                count = count + convertSpecial(parseInt(number.charAt(1)))
+                word = convertSpecial(parseInt(number.charAt(1)))
+                count.push(word)
                 continue //https://www.w3schools.com/jsref/jsref_continue.asp
             } else {
                 word = word + convertTens(parseInt(number))
-                count = count + word
+                count.push(word)
             }
+        } else if (number.length === 1) {
+            word = convertOnes(parseInt(number))
+            count.push(word)
         }
-        if (number.length === 1) {
-            count = count + convertOnes(parseInt(number))
-        }
-        
     }
-    let display = document.createElement("p")
-    display.innerText = count
-    document.body.append(display)
+    let size = count.length/10000
+    for (let i = 0; i < size; i++) {
+        let temp = count.slice(0, 10000)
+        count.splice(0, 10000)
+        setTimeout(displayWords, 3000, temp)
+    }
 }
 
-displayWords()
+//Function to display all numbers at once
+function displayWords(array) {
+        let display = document.createElement("p")
+        display.innerText = array.join(", ")
+        document.body.append(display)
+}
+
+//Function will use user input to find matching word
+function convertInput() {
+    let number = document.getElementById("inputNum").value
+    console.log(number)
+    let result = document.getElementById("result")
+    if (number < 1 || number > 1000000 || isNaN(number)) {
+        result.innerText = "Please enter valid number"
+    } else {
+        number = number.toString()
+        let word = ""
+        if (number.length === 7) {
+            result.innerText = "Result: one-million"
+        } else if (number.length >= 4 && number.length <= 6) {
+            word = convertThousands(parseInt(number))
+            result.innerText = "Result: " + word
+        } else if (number.length === 3) {
+            word = convertHundreds(parseInt(number))
+            result.innerText = "Result: " + word
+        } else if (number.length === 2) {
+            if (number.charAt(0) === "1") {
+                word = convertSpecial(parseInt(number.charAt(1)))
+                result.innerText = "Result: " + word
+            } else {
+                word = word + convertTens(parseInt(number))
+                result.innerText = "Result: " + word
+            }
+        } else if (number.length === 1) {
+            word = convertOnes(parseInt(number))
+            result.innerText = "Result: " + word
+        }
+    }
+}
+
+let inputNum = document.createElement("input")
+let title = document.createElement("label")
+let submit = document.createElement("button")
+let result = document.createElement("p")
+title.innerText = "Enter number between 1 and 1000000:"
+inputNum.id = "inputNum"
+result.id = "result"
+submit.innerText="Submit"
+submit.addEventListener("click", convertInput)
+document.body.append(title)
+document.body.append(inputNum)
+document.body.append(submit)
+document.body.append(result)
+
+countWords()
